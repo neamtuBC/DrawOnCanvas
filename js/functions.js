d22c993a8970d0834335906aca4324ea33a03d89
Bogdan <neamtu.bc@gmail.com>
2018-06-20 12:49:32 +0300

    -Buttons to select the style

--- js/functions.js
+++ js/functions.js
@@ -21,16 +21,39 @@ let isDrawing = false;
 // Position when draw starts
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
+let direction = true;
+let linear = true;
 
 // Function to draw
 function draw (e) {
-  if(!isDrawing) return ;
+  if(!isDrawing) {
+    return ;
+  }
   console.log(e);
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   ctx.beginPath();
   ctx.moveTo(lastX, lastY);
   ctx.lineTo(e.offsetX, e.offsetY);
   ctx.stroke();
   [lastX, lastY] = [e.offsetX, e.offsetY];
+  hue++;
+
+  if(hue>=360){
+    hue = 0;
+  }
+
+  if(ctx.lineWidth >= 75 || ctx.linewidth <= 1) {
+    direction = !direction;
+  }
+
+  if(!linear){
+    if(direction){
+      ctx.lineWidth++;
+    } else {
+      ctx.lineWidth = 75;
+    }
+  }
 }
 
 // Some listeners
@@ -38,6 +61,25 @@ canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mousedown', (e) => {
   isDrawing = true;
   [lastX, lastY] = [e.offsetX, e.offsetY];
+  direction = true;
+  ctx.lineWidth = 1;
 });
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
+
+
+// Button logic
+
+const progresiveButton = document.querySelector('#progBut');
+const uniformButton = document.querySelector('#uniBut');
+
+progresiveButton.addEventListener('click', () => {
+  linear = false;
+  uniformButton.className = "";
+  progresiveButton.className = "active";
+});
+uniformButton.addEventListener('click', () => {
+  linear = true;
+  progresiveButton.className = "";
+  uniformButton.className = "active";
+});
